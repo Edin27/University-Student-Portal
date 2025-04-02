803ef10b84a6a70c1c317d4728d693dfa4bc891c
Ibukunoluwa Akiode <ibukunakiode3@gmail.com>
2025-04-02 10:53:19 +0100

    Update FAQItem.java

--- src/main/java/model/FAQItem.java
+++ src/main/java/model/FAQItem.java
@@ -4,7 +4,8 @@ public class FAQItem {
     private final String question;
     private final String answer;
 
-    public FAQItem(String question, String answer) {
+    public FAQItem(int id, String question, String answer) {
+        this.id = id;
         this.question = question;
         this.answer = answer;
     }
@@ -16,4 +17,14 @@ public class FAQItem {
     public String getAnswer() {
         return answer;
     }
-}
+
+    public int getId() {
+        return id;
+    }
+
+    public String toString() {
+        return id + ". Q: " + question + " A: " + answer;
+    }
+} 
+
+
