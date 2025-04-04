79744b6650032833bbce3444be3b66811d0743ce
Jessie <s2484263@ed.ac.uk>
2025-04-05 00:01:13 +0100

    rearranged and corrected ConsultMemberOfStaffSystemTests a bit

--- src/test/java/system_tests/ConsultMemberOfStaffSystemTests.java
+++ src/test/java/system_tests/ConsultMemberOfStaffSystemTests.java
@@ -20,7 +20,9 @@ public class ConsultMemberOfStaffSystemTests extends TUITest {
     public void testInquiryNoTagSuccess() throws URISyntaxException, IOException, ParseException {
         setMockInput("student1@hindeburg.ac.uk", "inquiry topic", "This is an inquiry", "");
         View view = new TextUserInterface();
-        SharedContext sharedContext = new SharedContext(view);AuthenticationService auth = new MockAuthenticationService();EmailService email = new MockEmailService();
+        SharedContext sharedContext = new SharedContext(view);
+        AuthenticationService auth = new MockAuthenticationService();
+        EmailService email = new MockEmailService();
         InquirerController inquirerController = new InquirerController(sharedContext, view, auth, email);
         startOutputCapture();
         inquirerController.contactStaff();
@@ -31,7 +33,9 @@ public class ConsultMemberOfStaffSystemTests extends TUITest {
     public void testInquiryNoTagInvalidEmail() throws URISyntaxException, IOException, ParseException {
         setMockInput("wrongemail.com");
         View view = new TextUserInterface();
-        SharedContext sharedContext = new SharedContext(view);AuthenticationService auth = new MockAuthenticationService();EmailService email = new MockEmailService();
+        SharedContext sharedContext = new SharedContext(view);
+        AuthenticationService auth = new MockAuthenticationService();
+        EmailService email = new MockEmailService();
         InquirerController inquirerController = new InquirerController(sharedContext, view, auth, email);
         startOutputCapture();
         inquirerController.contactStaff();
@@ -42,18 +46,22 @@ public class ConsultMemberOfStaffSystemTests extends TUITest {
     public void testInquiryNoTagBlankSubject() throws URISyntaxException, IOException, ParseException {
         setMockInput("student1@hindeburg.ac.uk", "", "");
         View view = new TextUserInterface();
-        SharedContext sharedContext = new SharedContext(view);AuthenticationService auth = new MockAuthenticationService();EmailService email = new MockEmailService();
+        SharedContext sharedContext = new SharedContext(view);
+        AuthenticationService auth = new MockAuthenticationService();
+        EmailService email = new MockEmailService();
         InquirerController inquirerController = new InquirerController(sharedContext, view, auth, email);
         startOutputCapture();
         inquirerController.contactStaff();
-        assertOutputContains("Invalid email address! Please try again");
+        assertOutputContains("Inquiry subject cannot be blank! Please try again");
     }
 
     @Test
     public void TestInquiryTagSuccess() throws URISyntaxException, IOException, ParseException {
         setMockInput("student1@hindeburg.ac.uk", "inquiry topic", "This is an inquiry", "1");
         View view = new TextUserInterface();
-        SharedContext sharedContext = new SharedContext(view);AuthenticationService auth = new MockAuthenticationService();EmailService email = new MockEmailService();
+        SharedContext sharedContext = new SharedContext(view);
+        AuthenticationService auth = new MockAuthenticationService();
+        EmailService email = new MockEmailService();
         InquirerController inquirerController = new InquirerController(sharedContext, view, auth, email);
         sharedContext.getCourseManager().getCourses().add(new Course(view, "1", "course1",
                 "description", false, "org", "teacher1@hindeburg.ac.uk",
@@ -61,14 +69,17 @@ public class ConsultMemberOfStaffSystemTests extends TUITest {
         startOutputCapture();
         inquirerController.contactStaff();
         assertOutputContains("teacher1@hindeburg.ac.uk");
-        assertOutputContains("Your inquiry has been recorded. Someone will be in touch via email soon!");
+        assertOutputContains("Your inquiry has been recorded. Someone will be in touch " +
+                "via email soon!");
     }
 
     @Test
     public void TestInquiryTagFailure() throws URISyntaxException, IOException, ParseException {
         setMockInput("student1@hindeburg.ac.uk", "inquiry topic", "This is an inquiry", "2");
         View view = new TextUserInterface();
-        SharedContext sharedContext = new SharedContext(view);AuthenticationService auth = new MockAuthenticationService();EmailService email = new MockEmailService();
+        SharedContext sharedContext = new SharedContext(view);
+        AuthenticationService auth = new MockAuthenticationService();
+        EmailService email = new MockEmailService();
         InquirerController inquirerController = new InquirerController(sharedContext, view, auth, email);
         sharedContext.getCourseManager().getCourses().add(new Course(view, "1", "course1",
                 "description", false, "org", "teacher1@hindeburg.ac.uk",
