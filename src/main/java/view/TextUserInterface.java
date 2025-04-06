4fbfc345578214d2d962fef18fc88be379f9e57d
seanm3156 <seanm3156@gmail.com>
2025-04-06 17:32:25 +0100

    View course

--- src/main/java/view/TextUserInterface.java
+++ src/main/java/view/TextUserInterface.java
@@ -205,7 +205,7 @@ public class TextUserInterface implements View {
                             }
                             // check for clashes
                             for (Timetable.TimeSlot slot2 : slots) {
-                                if (!slot1.equals(slot2)) {
+                                if (!slot1.equals(slot2) && slot1.getStatus() == Timetable.Status.CHOSEN && slot2.getStatus() == Timetable.Status.CHOSEN) {
                                     if (slot1.overlaps(slot2.getStartDate(), slot2.getStartTime(), slot2.getEndDate(), slot2.getEndTime())) {
                                         if (courseManager.findCourse(slot1.getCourseCode()).isUnrecordedLecture(slot1.getActivityId())) {
                                             displayError("Unrecorded Lecture clash!\n");
